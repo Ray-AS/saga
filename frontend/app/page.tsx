4973390ca9f7a1cefe4ca79a2432491b9344a7f1
Ray-AS <abisuthans.business@gmail.com>
2026-01-06 01:22:35 -0500

    feat: add basic styling for / route

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,10 +3,9 @@ import { startGame } from "@/lib/start";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Saga</h1>
+    <main className="flex flex-col justify-center items-center gap-4">
       <form action={startGame}>
-        <button type="submit">Start</button>
+        <button type="submit" className="w-32 h-10 text-xl cursor-pointer shadow-md shadow-black/50 active:translate-y-0.5 active:shadow-zinc-900">Start</button>
       </form>
       <LoadGameButton />
     </main>
--- frontend/components/LoadGameButton.tsx
+++ frontend/components/LoadGameButton.tsx
@@ -8,6 +8,7 @@ export default function LoadGameButton() {
   return (
     <button
       onClick={() => router.push("/game")}
+      className="w-32 h-10 text-xl cursor-pointer shadow-md shadow-black/50 active:translate-y-0.5 active:shadow-zinc-900"
     >
       Load
     </button>
